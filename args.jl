2e4f7564f5c654e0305ce0955ef3ced41e63c28c
Jack <jarbus@tutanota.com>
2022-11-01 14:01:35 -0400

    Make arguments required to prevent propogation of nothing

--- args.jl
+++ args.jl
@@ -6,38 +6,51 @@ s = ArgParseSettings()
   "--window"
   nargs = 2
   arg_type = Int
+  required = true
   "--gx"
   arg_type = Int
+  required = true
   "--gy"
   arg_type = Int
+  required = true
   "--fires"
   nargs = '*'
   arg_type = Int
+  required = true
   "--foods"
   nargs = '*'
   arg_type = Int
+  required = true
   "--exp-name"
   arg_type = String
-  "--class-name"
-  arg_type = String
+  required = true
   "--model"
   arg_type = String
+  required = true
   "--num-gens"
   arg_type = Int
+  required = true
   "--day-steps"
   arg_type = Int
+  required = true
   "--batch-size"
   arg_type = Int
+  required = true
   "--episode-length"
   arg_type = Int
+  required = true
   "--num-agents"
   arg_type = Int
+  required = true
   "--pop-size"
   arg_type = Int
+  required = true
   "--alpha"
   arg_type = Float32
+  required = true
   "--mutation-rate"
   arg_type = Float32
+  required = true
   "--num-steps"
   "--checkpoint-interval"
   "--food-agent-start"
@@ -45,5 +58,6 @@ s = ArgParseSettings()
   "--light-coeff"
   "--pickup-coeff"
   "--food-types"
+  "--class-name"
 end
 args = parse_args(s)
