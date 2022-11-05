617c8872a1fb47404031c242aa2027fc2c4d14bd
Jack <jarbus@tutanota.com>
2022-11-05 12:35:07 -0400

    Faster loads

--- run-repl.jl
+++ run-repl.jl
@@ -1,6 +1,6 @@
+file = read(pipeline(`ls afiles`, `fzf`), String) |> strip
 using Revise
 includet("args.jl")
-file = read(pipeline(`ls afiles`, `fzf`), String) |> strip
 expname = ["--exp-name", file[1:end-4], "--local"] # get rid of .arg
 arg_vector = read("afiles/$file", String) |> split
 args = parse_args(vcat(arg_vector, expname), s)
