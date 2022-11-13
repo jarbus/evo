fce255d280c42e0bf185c23a461fdbd80aa751a7
Jack <jarbus@tutanota.com>
2022-11-12 20:15:47 -0500

    Save model

--- evotrade.jl
+++ evotrade.jl
@@ -2,6 +2,7 @@ using Distributed
 using Dates
 using DataFrames
 using CSV
+using FileIO
 using Infiltrator
 
 if !args["local"]
@@ -152,6 +153,7 @@ function main()
       else
         push!(df, mets)
       end
+      save("outs/$expname/models.jld2")
       CSV.write("outs/$expname/metrics.csv", df)
       avg_self_fit = (rew_dict["f0a0"] + rew_dict["f1a0"]) / 2
       println(logfile, "$(round(avg_self_fit, digits=2)) ")
