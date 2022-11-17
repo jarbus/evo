d64a7174be1d3895fd095b7687bbe3ca00317514
Jack <jarbus@tutanota.com>
2022-11-17 15:18:53 -0500

    Bugfix compute novelty

--- ga.jl
+++ ga.jl
@@ -22,8 +22,25 @@ end
 
 function compute_novelty(ind_bc::Vector{<:Float64}, archive_and_pop::Vector{Vector{Float64}})
     # Assumptions: Novelty against self is zero, ind_bc is in archive_and_pop
-    sum((ind_bc .- bc) .^ 2 for bc in archive_and_pop) / (length(archive_and_pop) - 1)
+    sum(sum((ind_bc .- bc) .^ 2) for bc in archive_and_pop) / (length(archive_and_pop) - 1)
 end
 
+function test_compute_novelty()
+    function gen_dist(len) 
+        x = rand(len)
+        x ./ sum(x)
+    end
+    archive = [gen_dist(9) for _ in 1:1000]
+    pop = [gen_dist(9) for _ in 1:1000]
+    archive_and_pop = vcat(archive, pop)
+    for ind_bc in pop
+        compute_novelty(ind_bc, archive_and_pop)
+    end
+    archive_and_pop = [[0.0, 1.0], [1.0, 0.0]]
+    ind = [1.0, 0.0]
+    nov = compute_novelty(ind, archive_and_pop)
+    @assert nov isa Float64
+    @assert nov == 2.0
+end
 
 end
