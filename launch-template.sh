e6ee3b8ba9d0b9371c4b0667bd061c5e181ecdfa
Jack <jarbus@tutanota.com>
2022-11-21 13:24:36 -0500

    Add quotes around args

--- launch-template.sh
+++ launch-template.sh
@@ -8,11 +8,12 @@
 #SBATCH --account=guest
 #SBATCH --time=24:00:00
 #SBATCH --partition=guest-compute
-#SBATCH --ntasks=25
+#SBATCH --ntasks=100
 #SBATCH --requeue
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=3GB
+#SBATCH --exclude=compute-9-[5-7]
 
 source /home/garbus/.bashrc
 conda activate trade
-julia run-script.jl $(cat /home/garbus/evotrade/afiles/${RUN_NAME}.arg) --exp-name ${RUN_NAME} --datime ${DATIME}
+julia run-script.jl $(cat /home/garbus/evotrade/afiles/${RUN_NAME}.arg) --exp-name "${RUN_NAME}" --datime "${DATIME}"
