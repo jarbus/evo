c9701ffb92ad5971a1037274c08c00ffbb25a6fb
Jack <jarbus@tutanota.com>
2022-11-30 10:26:28 -0500

    Fix indentation

--- estrade.jl
+++ estrade.jl
@@ -5,189 +5,188 @@ using FileIO
 using Infiltrator
 
 @everywhere begin
-  args = $args
-  args["local"] && using Revise
-  inc = args["local"] ? includet : include
-  inc("es.jl")
-  inc("net.jl")
-  inc("trade.jl")
-  inc("utils.jl")
-  inc("maze.jl")
-  @enum Env trade maze
-  env_type = !isempty(args["maze"]) ? Val(maze) : Val(trade)
+    args = $args
+    args["local"] && using Revise
+    inc = args["local"] ? includet : include
+    inc("es.jl")
+    inc("net.jl")
+    inc("trade.jl")
+    inc("utils.jl")
+    inc("maze.jl")
+    @enum Env trade maze
+    env_type = !isempty(args["maze"]) ? Val(maze) : Val(trade)
 end
 
 expname = args["exp-name"]
 @everywhere begin
-  using .DistributedES
-  using .Trade
-  using .Net
-  using .Utils
-  using .Maze
-  using Flux
-  using Statistics
-  using StableRNGs
+    using .DistributedES
+    using .Trade
+    using .Net
+    using .Utils
+    using .Maze
+    using Flux
+    using Statistics
+    using StableRNGs
 
 
-  env_config = mk_env_config(args)
-  
-  function run_batch(::Val{trade}, batch_size::Int, models::Dict{String,<:Chain}; evaluation=false, render_str::Union{Nothing,String}=nothing)
+    env_config = mk_env_config(args)
 
-    b_env = [Trade.PyTrade.Trade(env_config) for _ in 1:batch_size]
-    obs_size = (b_env[1].obs_size..., batch_size)
-    num_actions = b_env[1].num_actions
-    b_obs = batch_reset!(b_env, models)
-    max_steps = args["episode-length"] * args["num-agents"]
-    rews = Dict(key => 0.0f0 for key in keys(models))
-    for _ in 1:max_steps
-      b_obs, b_rew, b_dones, b_acts = batch_step!(b_env, models, b_obs, evaluation=evaluation)
-      for (b, rew_dict) in enumerate(b_rew)
-        for (name, rew) in rew_dict
-          rews[name] += rew
+    function run_batch(::Val{trade}, batch_size::Int, models::Dict{String,<:Chain}; evaluation=false, render_str::Union{Nothing,String}=nothing)
 
-          if render_str isa String && name == first(models).first
-            renderfile = "$render_str/b$b.out"
-            render(b_env[b], renderfile)
-          end
+        b_env = [Trade.PyTrade.Trade(env_config) for _ in 1:batch_size]
+        obs_size = (b_env[1].obs_size..., batch_size)
+        num_actions = b_env[1].num_actions
+        b_obs = batch_reset!(b_env, models)
+        max_steps = args["episode-length"] * args["num-agents"]
+        rews = Dict(key => 0.0f0 for key in keys(models))
+        for _ in 1:max_steps
+            b_obs, b_rew, b_dones, b_acts = batch_step!(b_env, models, b_obs, evaluation=evaluation)
+            for (b, rew_dict) in enumerate(b_rew)
+                for (name, rew) in rew_dict
+                    rews[name] += rew
+
+                    if render_str isa String && name == first(models).first
+                        renderfile = "$render_str/b$b.out"
+                        render(b_env[b], renderfile)
+                    end
+                end
+            end
         end
-      end
+        rew_dict = Dict(name => rew / batch_size for (name, rew) in rews)
+            mets = get_metrics(b_env)
+            rew_dict, mets, nothing
     end
-    rew_dict = Dict(name => rew / batch_size for (name, rew) in rews)
-    mets = get_metrics(b_env)
-    rew_dict, mets, nothing
-  end
 
-  function run_batch(::Val{maze}, batch_size::Int, models::Dict{String,<:Chain}; evaluation=false, render_str::Union{Nothing,String}=nothing)
+    function run_batch(::Val{maze}, batch_size::Int, models::Dict{String,<:Chain}; evaluation=false, render_str::Union{Nothing,String}=nothing)
 
-    reset!(env)
-    r = -Inf
-    for i in 1:args["episode-length"]
-        obs = get_obs(env)
-        obs = repeat(obs, outer=(1,1,1,batch_size))
-        probs = models["f0a0"](obs)
-        acts = sample_batch(probs)
-        @assert length(acts) == batch_size
-        r, done = step!(env, acts[1])
-        done && break
+        reset!(env)
+        r = -Inf
+        for i in 1:args["episode-length"]
+            obs = get_obs(env)
+            obs = repeat(obs, outer=(1,1,1,batch_size))
+            probs = models["f0a0"](obs)
+            acts = sample_batch(probs)
+            @assert length(acts) == batch_size
+            r, done = step!(env, acts[1])
+            done && break
+        end
+        rews = Dict("f0a0" => r, "f1a0"=> r)
+        bc = Dict("f0a0" => env.location, "f1a0"=> env.location)
+        rews, nothing, bc
     end
-    rews = Dict("f0a0" => r, "f1a0"=> r)
-    bc = Dict("f0a0" => env.location, "f1a0"=> env.location)
-    rews, nothing, bc
-  end
 
 
 
-  function fitness_pos(p1::Int, p2::Int)
-    models = Dict("f0a0" => re(θ .+ get_noise(nt, p1)),
-      "f1a0" => re(θ .+ get_noise(nt, p2)))
-    rew_dict, _, _ = run_batch(env_type, batch_size, models)
-    rew_dict["f0a0"], rew_dict["f1a0"]
-  end
-  function fitness_neg(p1::Int, p2::Int)
-    models = Dict("f0a0" => re(θ .- get_noise(nt, p1)),
-      "f1a0" => re(θ .- get_noise(nt, p2)))
-    rew_dict, _, _ = run_batch(env_type, batch_size, models)
-    rew_dict["f0a0"], rew_dict["f1a0"]
-  end
+    function fitness_pos(p1::Int, p2::Int)
+        models = Dict("f0a0" => re(θ .+ get_noise(nt, p1)),
+        "f1a0" => re(θ .+ get_noise(nt, p2)))
+        rew_dict, _, _ = run_batch(env_type, batch_size, models)
+        rew_dict["f0a0"], rew_dict["f1a0"]
+    end
+    function fitness_neg(p1::Int, p2::Int)
+        models = Dict("f0a0" => re(θ .- get_noise(nt, p1)),
+        "f1a0" => re(θ .- get_noise(nt, p2)))
+        rew_dict, _, _ = run_batch(env_type, batch_size, models)
+        rew_dict["f0a0"], rew_dict["f1a0"]
+    end
 
 end
 
 function main()
 
-  dt_str = args["datime"]
-  logname = "runs/$dt_str-$expname.log"
-  println("$expname")
-  df = nothing
-  @everywhere begin
-    opt = Adam(args["alpha"])
-    pop_size = args["pop-size"]
-    mut = args["mutation-rate"]
-    α = args["alpha"]
-    rng = StableRNG(0)
-    if "maze" in keys(args)
-        env = maze_from_file(args["maze"])
-    else
-        env = Trade.PyTrade.Trade(env_config)
-    end
-
-    batch_size = args["batch-size"]
-    θ, re = make_model(Symbol(args["model"]), (env.obs_size..., batch_size), env.num_actions) |> Flux.destructure
-    model_size = length(θ)
-  end
-  
-  # ###############
-  # load checkpoint
-  # ###############
-  check_name = "outs/$expname/check.jld2"
-  met_csv_name = "outs/$expname/metrics.csv"
-  start_gen = 1
-  # check if check exists on the file system
-  if isfile(check_name)
-    @assert isfile(met_csv_name)
-    check = load(check_name)
-    df = CSV.read(met_csv_name, DataFrame)
-    @everywhere θ = $check["theta"]
-    start_gen = check["gen"] + 1
-    println("resuming from gen $start_gen")
-  end
-
-  for i in start_gen:args["num-gens"]
-
-    if i % 3 == 0
-      # compute and write metrics and outfile
-      outdir = "outs/$expname/$i"
-      run(`mkdir -p $outdir`)
-      models = Dict("f0a0" => re(θ), "f1a0" => re(θ))
-      rew_dict, mets, _ = run_batch(env_type, batch_size, models, evaluation=false, render_str=outdir)
-      if !isnothing(mets)
-          df = update_df(df, mets)
-          CSV.write(met_csv_name, df)
-      end
-
-      # log mets and save gen
-      avg_self_fit = round((rew_dict["f0a0"] + rew_dict["f1a0"]) / 2, digits=2)
-      llog(islocal=args["local"], name=logname) do logfile
-        println(logfile, "Generation $i: $avg_self_fit")
-      end
-      !args["local"] && save(check_name, Dict("theta"=>θ,"gen"=>i))
-
-    end
-
+    dt_str = args["datime"]
+    logname = "runs/$dt_str-$expname.log"
+    println("$expname")
+    df = nothing
     @everywhere begin
-      nt = NoiseTable(rng, model_size, pop_size, mut)
+        opt = Adam(args["alpha"])
+        pop_size = args["pop-size"]
+        mut = args["mutation-rate"]
+        α = args["alpha"]
+        rng = StableRNG(0)
+        if "maze" in keys(args)
+            env = maze_from_file(args["maze"])
+        else
+            env = Trade.PyTrade.Trade(env_config)
+        end
+
+        batch_size = args["batch-size"]
+        θ, re = make_model(Symbol(args["model"]), (env.obs_size..., batch_size), env.num_actions) |> Flux.destructure
+        model_size = length(θ)
     end
 
-    # CHECK TO CONFIRM RNG IS SYNCHRONIZED
-    rands = [fetch(remotecall(() -> nt.noise[1], p)) for p in 1:nprocs()]
-    @assert length(unique(rands)) == 1
-
-
-    fut_pos = []
-    fut_neg = []
-
-    for p1 in 1:pop_size
-      #for p2 in 1:pop_size
-      p2 = p1
-      push!(fut_pos, remotecall(() -> fitness_pos(p1, p2), procs()[(p2%nprocs())+1]))
-      push!(fut_neg, remotecall(() -> fitness_neg(p1, p2), procs()[(p2%nprocs())+1]))
+    # ###############
+    # load checkpoint
+    # ###############
+    check_name = "outs/$expname/check.jld2"
+    met_csv_name = "outs/$expname/metrics.csv"
+    start_gen = 1
+    # check if check exists on the file system
+    if isfile(check_name)
+        check = load(check_name)
+        df = CSV.read(met_csv_name, DataFrame)
+        @everywhere θ = $check["theta"]
+        start_gen = check["gen"] + 1
+        println("resuming from gen $start_gen")
     end
 
-    fut_pos = [fetch(f)[1] for f in fut_pos]
-    fut_neg = [fetch(f)[1] for f in fut_neg]
+    for i in start_gen:args["num-gens"]
 
-    # Log fitness distribution
-    llog(islocal=args["local"], name=logname) do logfile
-      println(logfile, "min=$(min(fut_pos...)) mean=$(mean(fut_pos)) max=$(max(fut_pos...)) std=$(std(fut_pos))")
+        if i % 3 == 0
+            # compute and write metrics and outfile
+            outdir = "outs/$expname/$i"
+            run(`mkdir -p $outdir`)
+            models = Dict("f0a0" => re(θ), "f1a0" => re(θ))
+            rew_dict, mets, _ = run_batch(env_type, batch_size, models, evaluation=false, render_str=outdir)
+            if !isnothing(mets)
+                df = update_df(df, mets)
+                CSV.write(met_csv_name, df)
+            end
+
+            # log mets and save gen
+            avg_self_fit = round((rew_dict["f0a0"] + rew_dict["f1a0"]) / 2, digits=2)
+            llog(islocal=args["local"], name=logname) do logfile
+                println(logfile, "Generation $i: $avg_self_fit")
+            end
+            !args["local"] && save(check_name, Dict("theta"=>θ,"gen"=>i))
+
+        end
+
+        @everywhere begin
+            nt = NoiseTable(rng, model_size, pop_size, mut)
+        end
+
+        # CHECK TO CONFIRM RNG IS SYNCHRONIZED
+        rands = [fetch(remotecall(() -> nt.noise[1], p)) for p in 1:nprocs()]
+        @assert length(unique(rands)) == 1
+
+
+        fut_pos = []
+        fut_neg = []
+
+        for p1 in 1:pop_size
+            #for p2 in 1:pop_size
+            p2 = p1
+            push!(fut_pos, remotecall(() -> fitness_pos(p1, p2), procs()[(p2%nprocs())+1]))
+            push!(fut_neg, remotecall(() -> fitness_neg(p1, p2), procs()[(p2%nprocs())+1]))
+        end
+
+        fut_pos = [fetch(f)[1] for f in fut_pos]
+        fut_neg = [fetch(f)[1] for f in fut_neg]
+
+        # Log fitness distribution
+        llog(islocal=args["local"], name=logname) do logfile
+            println(logfile, "min=$(min(fut_pos...)) mean=$(mean(fut_pos)) max=$(max(fut_pos...)) std=$(std(fut_pos))")
+        end
+
+        F = fut_pos .- fut_neg
+        @assert length(F) == pop_size
+        ranks = Float32.(compute_centered_ranks(F))
+
+        @everywhere begin
+            grad = (args["l2"] * θ) - compute_grad(nt, $ranks) / (pop_size * mut)
+            Flux.Optimise.update!(opt, θ, grad)
+            @assert θ .|> isnan |> any |> !
+        end
     end
-
-    F = fut_pos .- fut_neg
-    @assert length(F) == pop_size
-    ranks = Float32.(compute_centered_ranks(F))
-
-    @everywhere begin
-      grad = (args["l2"] * θ) - compute_grad(nt, $ranks) / (pop_size * mut)
-      Flux.Optimise.update!(opt, θ, grad)
-      @assert θ .|> isnan |> any |> !
-    end
-  end
 end
--- gatrade.jl
+++ gatrade.jl
@@ -6,228 +6,228 @@ using FileIO
 using Infiltrator
 
 @everywhere begin
-  ts() = Dates.format(now(), "HH:MM:SS")*" "
-  args = $args
-  args["local"] && using Revise
-  inc = args["local"] ? includet : include
-  using Dates
-  println(ts() * "including modules")
-  inc("ga.jl")
-  println(ts() * "ga done")
-  inc("net.jl")
-  println(ts() * "net done")
-  inc("trade.jl")
-  println(ts() * "trade done")
-  inc("utils.jl")
-  println(ts() * "utils done")
-  inc("maze.jl")
-  println(ts() * "maze done")
+    ts() = Dates.format(now(), "HH:MM:SS")*" "
+    args = $args
+    args["local"] && using Revise
+    inc = args["local"] ? includet : include
+    using Dates
+    println(ts() * "including modules")
+    inc("ga.jl")
+    println(ts() * "ga done")
+    inc("net.jl")
+    println(ts() * "net done")
+    inc("trade.jl")
+    println(ts() * "trade done")
+    inc("utils.jl")
+    println(ts() * "utils done")
+    inc("maze.jl")
+    println(ts() * "maze done")
 
-  @enum Env trade maze
-  env_type = !isempty(args["maze"]) ? Val(maze) : Val(trade)
-  
+    @enum Env trade maze
+    env_type = !isempty(args["maze"]) ? Val(maze) : Val(trade)
+
 end
 
 expname = args["exp-name"]
 @everywhere begin
-  println("using modules")
-  using .Trade
-  using .Net
-  using .GANS
-  using .Utils
-  using .Maze
-  using Flux
-  using Statistics
-  using StableRNGs
+    println("using modules")
+    using .Trade
+    using .Net
+    using .GANS
+    using .Utils
+    using .Maze
+    using Flux
+    using Statistics
+    using StableRNGs
 
 
-  env_config = mk_env_config(args)
+    env_config = mk_env_config(args)
 
 
-  function fitness(p1::T, p2::T) where T<:Vector{<:UInt32}
-    models = Dict("f0a0" => re(reconstruct(p1, model_size, 0.01)),
-                  "f1a0" => re(reconstruct(p2, model_size, 0.01)))
-    rew_dict, _, bc = run_batch(env_type, batch_size, models)
-    rew_dict["f0a0"], rew_dict["f1a0"], bc["f0a0"], bc["f1a0"]
-  end
-
-  
-  function run_batch(::Val{maze}, batch_size::Int, models::Dict{String,<:Chain}; evaluation=false, render_str::Union{Nothing,String}=nothing)
-
-    reset!(env)
-    r = -Inf
-    for i in 1:args["episode-length"]
-        obs = get_obs(env)
-        probs = models["f0a0"](obs)
-        acts = sample_batch(probs)
-        @assert length(acts) == 1
-        r, done = step!(env, acts[1])
-        done && break
+    function fitness(p1::T, p2::T) where T<:Vector{<:UInt32}
+        models = Dict("f0a0" => re(reconstruct(p1, model_size, 0.01)),
+        "f1a0" => re(reconstruct(p2, model_size, 0.01)))
+        rew_dict, _, bc = run_batch(env_type, batch_size, models)
+        rew_dict["f0a0"], rew_dict["f1a0"], bc["f0a0"], bc["f1a0"]
     end
-    rews = Dict("f0a0" => r, "f1a0"=> r)
-    bc = Dict("f0a0" => env.location, "f1a0"=> env.location)
-    rews, nothing, bc
-  end
 
-  function run_batch(::Val{trade}, batch_size::Int, models::Dict{String,<:Chain}; evaluation=false, render_str::Union{Nothing,String}=nothing)
 
-    b_env = [Trade.PyTrade.Trade(env_config) for _ in 1:batch_size]
-    obs_size = (b_env[1].obs_size..., batch_size)
-    num_actions = b_env[1].num_actions
-    b_obs = batch_reset!(b_env, models)
-    max_steps = args["episode-length"] * args["num-agents"]
-    rews = Dict(key => 0.0f0 for key in keys(models))
-    total_acts = Dict(key => Vector{UInt32}() for key in keys(models))
-    for _ in 1:max_steps
-      b_obs, b_rew, b_dones, b_acts = batch_step!(b_env, models, b_obs, evaluation=evaluation)
-      for (b, rew_dict) in enumerate(b_rew)
-        for (name, rew) in rew_dict
+    function run_batch(::Val{maze}, batch_size::Int, models::Dict{String,<:Chain}; evaluation=false, render_str::Union{Nothing,String}=nothing)
 
-          total_acts[name] = vcat(total_acts[name], b_acts)
-          rews[name] += rew
-
-          if render_str isa String && name == first(models).first
-            renderfile = "$render_str/b$b.out"
-            render(b_env[b], renderfile)
-          end
+        reset!(env)
+        r = -Inf
+        for i in 1:args["episode-length"]
+            obs = get_obs(env)
+            probs = models["f0a0"](obs)
+            acts = sample_batch(probs)
+            @assert length(acts) == 1
+            r, done = step!(env, acts[1])
+            done && break
         end
-      end
+        rews = Dict("f0a0" => r, "f1a0"=> r)
+        bc = Dict("f0a0" => env.location, "f1a0"=> env.location)
+        rews, nothing, bc
     end
-    rew_dict = Dict(name => rew / batch_size for (name, rew) in rews)
-    mets = get_metrics(b_env)
-    bc = Dict(name => bc1(total_acts[name], num_actions) for (name, _) in models)
-    rew_dict, mets, bc
-  end
+
+    function run_batch(::Val{trade}, batch_size::Int, models::Dict{String,<:Chain}; evaluation=false, render_str::Union{Nothing,String}=nothing)
+
+        b_env = [Trade.PyTrade.Trade(env_config) for _ in 1:batch_size]
+        obs_size = (b_env[1].obs_size..., batch_size)
+        num_actions = b_env[1].num_actions
+        b_obs = batch_reset!(b_env, models)
+        max_steps = args["episode-length"] * args["num-agents"]
+        rews = Dict(key => 0.0f0 for key in keys(models))
+        total_acts = Dict(key => Vector{UInt32}() for key in keys(models))
+        for _ in 1:max_steps
+            b_obs, b_rew, b_dones, b_acts = batch_step!(b_env, models, b_obs, evaluation=evaluation)
+            for (b, rew_dict) in enumerate(b_rew)
+                for (name, rew) in rew_dict
+
+                    total_acts[name] = vcat(total_acts[name], b_acts)
+                    rews[name] += rew
+
+                    if render_str isa String && name == first(models).first
+                        renderfile = "$render_str/b$b.out"
+                        render(b_env[b], renderfile)
+                    end
+                end
+            end
+        end
+        rew_dict = Dict(name => rew / batch_size for (name, rew) in rews)
+        mets = get_metrics(b_env)
+        bc = Dict(name => bc1(total_acts[name], num_actions) for (name, _) in models)
+        rew_dict, mets, bc
+    end
 end
 
 function main()
-  dt_str = args["datime"]
-  logname="runs/$dt_str-$expname.log"
-  println("$expname")
-  df = nothing
-  @everywhere begin
-    pop_size = args["pop-size"]
-    T = args["num-elites"]
-    mut = args["mutation-rate"]
-    if "maze" in keys(args)
-        env = maze_from_file(args["maze"])
-    else
-        env = Trade.PyTrade.Trade(env_config)
-    end
-    batch_size = args["batch-size"]
-    θ, re = make_model(Symbol(args["model"]), (env.obs_size..., batch_size), env.num_actions, false) |> Flux.destructure
-    model_size = length(θ)
-  end
-
-  pop = Vector{Vector{UInt32}}()
-  next_pop = Vector{Vector{UInt32}}()
-  best = (-Inf, [])
-  archive = Set()
-  BC = nothing
-  F = nothing
-
-  # ###############
-  # load checkpoint
-  # ###############
-  check_name = "outs/$expname/check.jld2"
-  met_csv_name = "outs/$expname/metrics.csv"
-  start_gen = 1
-  # check if check exists on the file system
-  if isfile(check_name)
-    @assert isfile(met_csv_name)
-    df = CSV.read(met_csv_name, DataFrame)
-    check = load(check_name)
-    start_gen = check["gen"] + 1
-    F = check["F"]
-    BC = check["BC"]
-    best = check["best"]
-    archive = check["archive"]
-    pop = check["pop"]
-    next_pop = copy(pop)
-    @assert length(pop) == pop_size
-    println(ts()*"resuming from gen $start_gen")
-  end
-
-  for g in start_gen:args["num-gens"]
-
-    i₀ = g==1 ? 1 : 2
-    # run on first gen
-    for i in i₀:pop_size
-      if g == 1
-        push!(next_pop, [rand(UInt32)])
-      else
-        k = (rand(UInt) % T) + 1 # select parent
-        next_pop[i] = copy(pop[k])
-        push!(next_pop[i], rand(UInt32))
-      end
-    end
-    @assert length(next_pop) == pop_size
-    pop = next_pop 
-
-    futs = []
-    for p1 in i₀:pop_size
-      p2 = p1
-      push!(futs, remotecall(() -> fitness(pop[p1], pop[p2]), procs()[(p2%nprocs())+1]))
-    end
-    println(ts()*"fetching")
-    fetches = [fetch(fut) for fut in futs]
-    println(ts()*"fetched")
-   
-    if g==1
-        F = [(fet[1]+fet[2])/2 for fet in fetches]
-        BC = [fet[3] for fet in fetches]
-    else
-        F  = vcat([F[1]],  [fet[1]+fet[2]/2 for fet in fetches])
-        BC = vcat([BC[1]], [fet[3] for fet in fetches])
-    end
-
-    @assert length(F) == length(BC) == pop_size
-    max_fit = max(F...)
-    if max_fit > best[1]
-        llog(islocal=args["local"], name=logname) do logfile
-            println(logfile, ts()*"New best ind found, F=$max_fit")
+    dt_str = args["datime"]
+    logname="runs/$dt_str-$expname.log"
+    println("$expname")
+    df = nothing
+    @everywhere begin
+        pop_size = args["pop-size"]
+        T = args["num-elites"]
+        mut = args["mutation-rate"]
+        if "maze" in keys(args)
+            env = maze_from_file(args["maze"])
+        else
+            env = Trade.PyTrade.Trade(env_config)
         end
-        best = (max_fit, pop[argmax(F)])
+        batch_size = args["batch-size"]
+        θ, re = make_model(Symbol(args["model"]), (env.obs_size..., batch_size), env.num_actions, false) |> Flux.destructure
+        model_size = length(θ)
     end
-    for i in 1:pop_size
-        if i > 1 && rand() > 0.01
-            push!(archive, (BC[i], pop[i]))
+
+    pop = Vector{Vector{UInt32}}()
+    next_pop = Vector{Vector{UInt32}}()
+    best = (-Inf, [])
+    archive = Set()
+    BC = nothing
+    F = nothing
+
+    # ###############
+    # load checkpoint
+    # ###############
+    check_name = "outs/$expname/check.jld2"
+    met_csv_name = "outs/$expname/metrics.csv"
+    start_gen = 1
+    # check if check exists on the file system
+    if isfile(check_name)
+        @assert isfile(met_csv_name)
+        df = CSV.read(met_csv_name, DataFrame)
+        check = load(check_name)
+        start_gen = check["gen"] + 1
+        F = check["F"]
+        BC = check["BC"]
+        best = check["best"]
+        archive = check["archive"]
+        pop = check["pop"]
+        next_pop = copy(pop)
+        @assert length(pop) == pop_size
+        println(ts()*"resuming from gen $start_gen")
+    end
+
+    for g in start_gen:args["num-gens"]
+
+        i₀ = g==1 ? 1 : 2
+        # run on first gen
+        for i in i₀:pop_size
+            if g == 1
+                push!(next_pop, [rand(UInt32)])
+            else
+                k = (rand(UInt) % T) + 1 # select parent
+                next_pop[i] = copy(pop[k])
+                push!(next_pop[i], rand(UInt32))
+            end
+        end
+        @assert length(next_pop) == pop_size
+        pop = next_pop 
+
+        futs = []
+        for p1 in i₀:pop_size
+            p2 = p1
+            push!(futs, remotecall(() -> fitness(pop[p1], pop[p2]), procs()[(p2%nprocs())+1]))
+        end
+        println(ts()*"fetching")
+        fetches = [fetch(fut) for fut in futs]
+        println(ts()*"fetched")
+
+        if g==1
+            F = [(fet[1]+fet[2])/2 for fet in fetches]
+            BC = [fet[3] for fet in fetches]
+        else
+            F  = vcat([F[1]],  [fet[1]+fet[2]/2 for fet in fetches])
+            BC = vcat([BC[1]], [fet[3] for fet in fetches])
+        end
+
+        @assert length(F) == length(BC) == pop_size
+        max_fit = max(F...)
+        if max_fit > best[1]
+            llog(islocal=args["local"], name=logname) do logfile
+                println(logfile, ts()*"New best ind found, F=$max_fit")
+            end
+            best = (max_fit, pop[argmax(F)])
+        end
+        for i in 1:pop_size
+            if i > 1 && rand() > 0.01
+                push!(archive, (BC[i], pop[i]))
+            end
+        end
+
+        pop_and_arch_bc = vcat([bc for (bc, _) in archive], BC)
+        @assert length(pop_and_arch_bc) == length(archive) + pop_size
+        novelties = [compute_novelty(bc, pop_and_arch_bc) for bc in BC]
+        @assert length(novelties) == pop_size
+
+        order = sortperm(novelties, rev=true)
+        pop = pop[order]
+        BC = BC[order]
+        F =  F[order]
+        @assert length(pop) == length(BC) == length(F) == pop_size
+
+        # LOG
+        if g % 1 == 0
+            models = Dict("f0a0" => re(reconstruct(pop[1], model_size)),
+            "f1a0" => re(reconstruct(pop[1], model_size)))
+
+            # Compute and write metrics
+            outdir = "outs/$expname/$g"
+            run(`mkdir -p $outdir`)
+            rew_dict, mets, _ = run_batch(env_type, batch_size, models, evaluation=false, render_str=outdir)
+            if !isnothing(mets)
+                df = update_df(df, mets)
+                CSV.write(met_csv_name, df)
+            end
+
+            # Log to file
+            avg_self_fit = round((rew_dict["f0a0"] + rew_dict["f1a0"]) / 2; digits=2)
+            llog(islocal=args["local"], name=logname) do logfile
+                println(logfile, ts()*"Generation $g: $avg_self_fit")
+            end
+
+            # Save checkpoint
+            !args["local"] && save(check_name, Dict("gen"=>g, "pop"=>pop, "archive"=>archive, "BC"=> BC, "F"=>F, "best"=>best))
         end
     end
-
-    pop_and_arch_bc = vcat([bc for (bc, _) in archive], BC)
-    @assert length(pop_and_arch_bc) == length(archive) + pop_size
-    novelties = [compute_novelty(bc, pop_and_arch_bc) for bc in BC]
-    @assert length(novelties) == pop_size
-
-    order = sortperm(novelties, rev=true)
-    pop = pop[order]
-    BC = BC[order]
-    F =  F[order]
-    @assert length(pop) == length(BC) == length(F) == pop_size
-
-    # LOG
-    if g % 1 == 0
-      models = Dict("f0a0" => re(reconstruct(pop[1], model_size)),
-          "f1a0" => re(reconstruct(pop[1], model_size)))
-
-      # Compute and write metrics
-      outdir = "outs/$expname/$g"
-      run(`mkdir -p $outdir`)
-      rew_dict, mets, _ = run_batch(env_type, batch_size, models, evaluation=false, render_str=outdir)
-      if !isnothing(mets)
-        df = update_df(df, mets)
-        CSV.write(met_csv_name, df)
-      end
-
-      # Log to file
-      avg_self_fit = round((rew_dict["f0a0"] + rew_dict["f1a0"]) / 2; digits=2)
-      llog(islocal=args["local"], name=logname) do logfile
-      println(logfile, ts()*"Generation $g: $avg_self_fit")
-      end
-
-      # Save checkpoint
-      !args["local"] && save(check_name, Dict("gen"=>g, "pop"=>pop, "archive"=>archive, "BC"=> BC, "F"=>F, "best"=>best))
-    end
-  end
 end
