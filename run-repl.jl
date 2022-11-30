8db0756075be7e785fe830b93ff84e9a4290dd78
Jack <jarbus@tutanota.com>
2022-11-29 23:36:33 -0500

    better string concat

--- run-repl.jl
+++ run-repl.jl
@@ -5,4 +5,4 @@ includet("args.jl")
 expname = ["--exp-name", file[1:end-4], "--local", "--datime", Dates.format(now(), "mm-dd_HH:MM")] # get rid of .arg
 arg_vector = read("afiles/$file", String) |> split
 args = parse_args(vcat(arg_vector, expname), s)
-includet(string(args["algo"],"trade.jl"))
+includet(args["algo"]*"trade.jl")
