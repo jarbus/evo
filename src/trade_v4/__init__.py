695bd5a8d9efb89f70100b0d4b55f6cfa1f990d7
Jack <jarbus@tutanota.com>
2022-12-07 21:20:37 -0500

    rm: ray requirements from trade_v4

--- src/trade_v4/__init__.py
+++ src/trade_v4/__init__.py
@@ -1,1 +1,1 @@
-from .trade_v4 import Trade, METABOLISM, PLACE_AMOUNT, TradeCallback, POLICY_MAPPING_FN, FireCornerSpawner, FoodSpawner
+from .trade_v4 import Trade, METABOLISM, PLACE_AMOUNT, POLICY_MAPPING_FN, FireCornerSpawner, FoodSpawner
--- src/trade_v4/trade_v4/__init__.py
+++ src/trade_v4/trade_v4/__init__.py
@@ -1,4 +1,3 @@
 from .trade_env import Trade, METABOLISM, PLACE_AMOUNT
-from .trade_callback import TradeCallback
 from .utils import POLICY_MAPPING_FN
 from .spawners import FireCornerSpawner, FoodSpawner 
