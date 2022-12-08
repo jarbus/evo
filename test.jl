c7327e6f764baf254bbe6630e93a24faa1b8c61e
Jack <jarbus@tutanota.com>
2022-12-07 21:23:09 -0500

    fix: main() is now called when loading test.jl

--- test.jl
+++ test.jl
@@ -10,10 +10,10 @@ function t()
     # file = "test/test_trade.jl"
     file = "test/test_rollout.jl"
     includet(file)
+    main()
     roc([file], [EvoTrade]) do
         println("Running main()")
         main()
     end
 end
-main()
 t()
