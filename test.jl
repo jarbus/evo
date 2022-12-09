1e73fe3482a7110ed54683a971f8bf6423f96568
Jack <jarbus@tutanota.com>
2022-12-09 17:04:28 -0500

    fix: more consistent test.jl implementation

--- test.jl
+++ test.jl
@@ -6,16 +6,15 @@ using StableRNGs
 using Test
 
 
-file = "test/test_noisetable.jl"
-# file = "test/test_ga.jl"
+# file = "test/test_noisetable.jl"
 # file = "test/test_net.jl"
 # file = "test/test_maze.jl"
 # file = "test/test_trade.jl"
 # file = "test/test_rollout.jl"
-includet(file)
-function t()
+# file = "test/test_ga.jl"
+function t(file::String)
     roc([file], [EvoTrade]) do
         println("Running main()")
-        main()
+        include(file)
     end
 end
