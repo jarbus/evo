9ea3cd845e16c6cb79bb51c5d3fef3d803e1c8a6
Jack <jarbus@tutanota.com>
2023-01-03 10:08:36 -0500

    chg: multiproc visualizations

--- run-batch-vis.sh
+++ run-batch-vis.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # shellcheck disable=SC2206
-#SBATCH --output=/dev/null
+#SBATCH --output=/home/garbus/evotrade/run-batch-vis.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
 
  for out in $1/*.out ; do
-    python /home/garbus/evotrade/s2g.py "$out"
+    python /home/garbus/evotrade/s2g.py "$out" &
  done
 
+wait
