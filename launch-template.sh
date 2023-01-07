d9d026f387863fc1faa4f5551c258ae25d1b1ddc
Jack <jarbus@tutanota.com>
2023-01-07 13:43:33 -0500

    add: error file, exclude poisoned nodes

--- launch-template.sh
+++ launch-template.sh
@@ -5,6 +5,7 @@
 #SBATCH --mail-user=9147037394@vtext.com
 #SBATCH --job-name=${RUN_NAME}
 #SBATCH --output=/home/garbus/evotrade/runs/${CLS_NAME}/${JOB_NAME}.std
+#SBATCH --error=/home/garbus/evotrade/runs/${CLS_NAME}/${JOB_NAME}.err
 #SBATCH --account=guest
 #SBATCH --time=24:00:00
 #SBATCH --partition=guest-compute
@@ -12,6 +13,7 @@
 #SBATCH --requeue
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=6GB
+#SBATCH --exclude=compute-5-12,compute-7-0
 
 source /home/garbus/.bashrc
 conda activate trade
